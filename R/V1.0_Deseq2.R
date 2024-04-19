1617c05343448d887952023db1c0603fa600b131
Steffi_Rose <54937354+Steffiroseck@users.noreply.github.com>
2024-04-19 15:08:13 +0100

    Update V1.0_Deseq2.R

--- R/V1.0_Deseq2.R
+++ R/V1.0_Deseq2.R
@@ -21,8 +21,6 @@ setwd("/mnt/sda1/00_fastq/Sheep/")
 # create a directory to store DESeq2 outputs. Since I am pre-filtering reads in coming steps, 
 # I am creating directories to keep stringent results (where pre-filtering is done), and relaxed ( where no pre-filtering applied).
 system("mkdir 6.deseq2")
-#system("mkdir 6.deseq2/stringent")
-#system("mkdir 6.deseq2/relaxed")
 
 # Read the raw count data generated from featureCounts
 countData<-read.csv("5.featurecounts/Lambs.featurecounts.hisat2.Rmatrix",sep="\t", header=T, check.names=F)
@@ -64,22 +62,22 @@ countData <- countData[,unique(rownames(metaData))]
 all(colnames(countData) == rownames(metaData))
 
 # Users should center and scale numeric variables in the design to improve GLM convergence if using numeric variables (recommended by deseq2 vignette)
-# metaData$CH4production=scale(metaData$CH4production, center=TRUE) 
+metaData$CH4production=scale(metaData$CH4production, center=TRUE) 
 deseq2Data <- DESeqDataSetFromMatrix(countData=countData, colData=metaData, design= ~CH4production)
 
 # Stringent approach where we keep only rows that have at least 10 reads total
 keep <- rowSums(counts(deseq2Data)) >= 10
-deseq2Data <- deseq2Data[keep,] #22,750 remaining
+deseq2Data <- deseq2Data[keep,] #22,753 remaining
 
 deseq2Data <- DESeq(deseq2Data)
-
+dim(deseq2Data)
 
 # loop through results and extract significant DEGs for each model term
 # specify the cut-offs for pval and lfc in the below variables.
 # make sure to change the filenames with the cutoff values before saving the deg file (Line 91)
 
-pval = 0.1
-lfc = 0
+pval = 0.05
+lfc = 0.584
 results = resultsNames(deseq2Data)
 upresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"upDEGs"))
 downresultstable = matrix(nrow = length(results), ncol = 1, dimnames = list(results,"downDEGs"))
@@ -92,7 +90,7 @@ for(i in 1:length(results)){
   upDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange > lfc))))
   downDEGs = (length(na.omit(which(res$padj<pval & res$log2FoldChange < -lfc))))
   resSig = subset(resorder, padj < pval & log2FoldChange > lfc | padj < pval & log2FoldChange < -lfc)
-  write.csv(resSig , file=paste0("6.deseq2/",results[i],".0.1P.0.584LFC.updownDEGs.csv"), row.names = T)
+  write.csv(resSig , file=paste0("6.deseq2/",results[i],".0.05P.0.584LFC.updownDEGs.csv"), row.names = T)
   upresultstable[results[i],"upDEGs"] = upDEGs
   downresultstable[results[i],"downDEGs"] = downDEGs 
 }
@@ -122,11 +120,11 @@ EnhancedVolcano(res,
 	x='log2FoldChange',
 	y='padj',
 	title='Volcano plot',
-	caption = 'FC cutoff, 0; p-value cutoff, 0.1',
-	pCutoff=0.1,
-	FCcutoff=0,
-	pointSize = 3.0,
-	labSize=2,legendPosition = 'top',legendLabSize = 12)
+	caption = 'FC cutoff, 0.584; p-value cutoff, 0.05',
+	pCutoff=0.05,
+	FCcutoff=0.584,
+	pointSize = 5.0,
+	labSize=5,legendPosition = 'top',legendLabSize = 12)
 dev.off()
 
 ##labSize=2,legendPosition = 'top',legendLabSize = 12 for volcanoplot if need to add gene names
